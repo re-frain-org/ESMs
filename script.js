64070b9c56d5286b51484692b5ce5916950acdfe
Re Frain (extends 黐麟) <35806750+re-frain-org@users.noreply.github.com>
2019-06-05 18:55:55 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+import Pointer from "./modules/Pointer.js";
+customElements.define("x-pointer", Pointer);
+const container=document.createElement("div");
+container.style.height="200px";
+container.style.width="200px";
+document.body.appendChild(container);
+const pointer=new Pointer();
+pointer.init(container, 20);
